91c4480c50f9f797a620d73e8d702f31266ad737
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-04 19:32:25 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,26 +6,26 @@ def array_with_two_elements
   new_array = ["cat","dog"]
 end
 
- def first_element(arr)
+ def first_element(array)
   arr[0]
 end
 
- def third_element(arr)
+ def third_element(array)
   arr[2]
 end
 
- def last_element(arr)
+ def last_element(array)
   arr[-1]
 end
 
- def first_element_with_array_methods(arr)
+ def first_element_with_array_methods(array)
   arr.first
 end
 
- def last_element_with_array_methods(arr)
+ def last_element_with_array_methods(array)
   arr.last
 end
 
- def length_of_array(arr)
+ def length_of_array(array)
   arr.length
 end 
